bdc28cfb9d4b7cdd6bca19498aede01bbfc8dee1
lonzohope <hopemutheu@gmail.com>
2019-05-13 14:40:32 +0300

    implemented the image methods

--- photo/models.py
+++ photo/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime as dt
+
 
 class Location(models.Model):
     name = models.CharField(max_length = 25)
@@ -20,10 +22,20 @@ class Images(models.Model):
     image_location = models.ForeignKey(Location)
     image_category = models.ForeignKey(Category)  
    
-   
-
-    def save_editor(self):
-        self.save()
-    
+    def __str__(self):
+        return self.image_name
     class Meta:
         ordering = ['image']
+
+    def save_image(self):
+        self.save()
+
+    @classmethod
+    def get_image_by_id(cls):
+        images = cls.objects.get(pk=id)
+        return images
+
+    @classmethod
+    def search_by_image_category(cls,search_term):
+        images = cls.objects.filter(image_icontains=search_terms)
+        return images 
--- photo/views.py
+++ photo/views.py
@@ -3,6 +3,7 @@ from django.http  import HttpResponse,Http404
 import datetime as dt
 
 
+
 def welcome(request):
     return render(request, 'welcome.html')
 
