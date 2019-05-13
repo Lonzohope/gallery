bdbe3f26fdfd1abfb834e9f59be8a9cdb6110b20
lonzohope <hopemutheu@gmail.com>
2019-05-13 11:51:44 +0300

    ran tests

--- photo/models.py
+++ photo/models.py
@@ -1,22 +1,27 @@
 from django.db import models
 
+class Location(models.Model):
+    name = models.CharField(max_length = 25)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length = 25)
+
+    def __str__(self):
+        return self.name
+
 class Images(models.Model):
     image = models.ImageField()
     image_name = models.CharField(max_length =30)
     image_description = models.CharField(max_length =30)
-    image_location = models.ImageField()
-    image_category = models.ImageField()  
+    image_location = models.ForeignKey(Location)
+    image_category = models.ForeignKey(Category)  
    
-   def __str__(self):
+    def __str__(self):
         return self.image
 
     
     class Meta:
         ordering = ['image']
-    
-
-   try:
-        images = Image.objects.get(image)
-        print('Images found')
-      except DoesNotExist:
-          print('Images was not found')
--- photo/tests.py
+++ photo/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Images
 
-# Create your tests here.
+class ImagesTestClass(TestCase):
+
+    # Set up method
+       def setUp(self):
+        self.image= Images()
+
+        #Testing instance
+        def test_instance(self):
+         self.assertTrue(isinstance(self.image,Editor))  
+         
+
+        # Testing Save Method
+        def test_save_method(self):
+            self.image.save_images()
+            images = Images.objects.all()
+            self.assertTrue(len(images) > 0)
