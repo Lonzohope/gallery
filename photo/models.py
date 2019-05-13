8156724995c0dce95ce217fe53d486a6f3646e71
lonzohope <hopemutheu@gmail.com>
2019-05-13 11:13:28 +0300

    created an image model

--- photo/models.py
+++ photo/models.py
@@ -6,3 +6,17 @@ class Images(models.Model):
     image_description = models.CharField(max_length =30)
     image_location = models.ImageField()
     image_category = models.ImageField()  
+   
+   def __str__(self):
+        return self.image
+
+    
+    class Meta:
+        ordering = ['image']
+    
+
+   try:
+        images = Image.objects.get(image)
+        print('Images found')
+      except DoesNotExist:
+          print('Images was not found')
