143993163c1a62070641409fc5a7979452078c52
lonzohope <hopemutheu@gmail.com>
2019-05-13 07:44:01 +0300

    add changes to the files made and also created a template folder to display our welcome page

--- photo/views.py
+++ photo/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
 import datetime as dt
 
+
 def welcome(request):
-    return HttpResponse('Welcome to the Hub')
+    return render(request, 'Welcome.html')
 
 
 def photo_of_day(request):
@@ -40,4 +41,28 @@ def photo_of_day(request):
     html = f'''
         <html>
           <body>
-            <h1>Photo for {day}
+            <h1>Photo for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+          </html>
+            '''
+          return HttpResponse(html)
+
+
+def past_days_photo(request,past_date):
+
+      try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+          
+        except ValueError:
+           # Raise 404 error when ValueError is thrown raise Http404()
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Photo for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
